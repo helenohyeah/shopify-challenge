762996265945bcc694e62a0a11011329f531b82b
helenohyeah <ouyang.helen@gmail.com>
2021-01-11 17:33:43 -0500

    Create basic components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 'use strict';
 
+
+function Header() {
+  return (
+    <header>
+      <h1>The Shoppies</h1>
+    </header>
+  );
+}
+
+function Search() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+
+  return (
+    <div className="search">
+      <label htmlFor="search">Movie Title:</label>
+      <input 
+        type="text"
+        name="search"
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Results() {
+  return (
+    <div className="results">
+      <h2>Results</h2>
+    </div>
+  );
+}
+
+function Nominations() {
+  return (
+    <div className="nominations">
+      <h2>Nominations</h2>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div className="container">
+    <Header />
+    <Search />
+    <Results />
+    <Nominations />
+  </div>
+  ,
   document.getElementById('root')
 );

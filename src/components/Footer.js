6ac382227de764eb435c664abe15817cf7eec42d
helenohyeah <ouyang.helen@gmail.com>
2021-01-15 22:36:44 -0500

    Add and style footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      Handcrafted by <a href='https://helenouyang.me' target='_blank' rel='noopener noreferer'>Helen Ouyang</a> © 2021 | <a href='https://github.com/helenohyeah/shopify-challenge' target='_blank' rel='noopener noreferer'>View Source</a>
+    </footer>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@
 // Components
 import Header from './components/Header.js';
 import Main from './components/Main.js';
+import Footer from './components/Footer.js';
 
 ReactDOM.render(
-  <div>
+  <div className='container'>
     <Header />
     <Main />
+    <Footer />
   </div>
   ,
   document.getElementById('root')
